c29e8875c28a8560e1f0009c5d4c27fc8319d368
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-12-23 22:27:10 +0700

    inisialisasi projek restaurant management

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.UserRouter(router)
+	router.Use(middleware.Authentication())
+
+	router.FoodRoutes(router)
+}
